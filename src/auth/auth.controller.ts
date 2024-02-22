da92ffaa0af98c085e61d31d28bb27a6bcae1cc9
Alberto Polo <bertopolo91@gmail.com>
2024-02-22 15:24:22 +0100

    first endpoints to test

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
-@Controller()
-export class AuthController {}
+@Controller('auth') //here you define the main endpoint name
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('signup') // /auth/signup
+  signup() {
+    return 'I am signed up';
+  }
+
+  @Post('signin') // /auth/signin
+  signin() {
+    return 'I am signed in';
+  }
+}
