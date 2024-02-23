a8e749e2cb319d7d30f1549ef89323a5e9722670
Alberto Polo <bertopolo91@gmail.com>
2024-02-23 08:36:15 +0100

    handle error: duplicated email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -9,26 +10,35 @@ export class AuthService {
   async signup(dto: AuthDto) {
     // generate the password hash
     const hash = await argon.hash(dto.password);
-
-    // save the new user in the database
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash, // why this is not password : hash ????
-      },
-      // select: {
-      //   // inside here you choose what you want to return. cause for eg, you dont wnat to return the hash
-      //   id: true,
-      //   email: true,
-      //   createdAt: true,
-      //   updatedAt: true,
-      // },
-    });
-    // instead of select:{}, you can write : delete user.hash  and is quicker
-    delete user.hash;
-
-    // return the saved user
-    return user;
+    try {
+      // save the new user in the database
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash, // why this is not password : hash ????
+        },
+        // select: {
+        //   // inside here you choose what you want to return. cause for eg, you dont wnat to return the hash
+        //   id: true,
+        //   email: true,
+        //   createdAt: true,
+        //   updatedAt: true,
+        // },
+      });
+      // instead of select:{}, you can write : delete user.hash  and is quicker
+      delete user.hash;
+      // return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        // basicaly if its a Prisma error
+        //we only need the prisma error for duplicated emails
+        if (error.code === 'P2002')
+          throw new ForbiddenException('Credentials are already in use');
+      }
+      //not a Prisma error
+      throw error;
+    }
   }
 
   signin() {}
